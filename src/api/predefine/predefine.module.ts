18b8412480a4719791b133db63ca39037b750dec
Note <note_kap@hotmail.com>
2024-09-12 17:41:21 +0700

    Refactor predefine module to use updated entity import

--- src/api/predefine/predefine.module.ts
+++ src/api/predefine/predefine.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CommonService } from 'src/common/common.service';
-import { co_Predefine } from 'src/entity/predefine.entity';
+import { Predefine } from 'src/entity/predefine.entity';
 import { PredefineController } from './predefine.controller';
 import { PredefineService } from './predefine.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([co_Predefine])],
+  imports: [TypeOrmModule.forFeature([Predefine])],
   exports: [TypeOrmModule],
   controllers: [PredefineController],
   providers: [PredefineService, CommonService],
