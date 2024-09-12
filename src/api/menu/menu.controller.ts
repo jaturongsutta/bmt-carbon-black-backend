f43a7961083f558ae05b4f6977fedb690ccbdb13
Note <note_kap@hotmail.com>
2024-09-12 16:58:03 +0700

    Refactor menu routes to use common-master folder structure

---
+++ src/api/menu/menu.controller.ts
@@ -0,0 +1,100 @@
+import {
+  Request,
+  Controller,
+  Get,
+  Post,
+  Param,
+  Body,
+  Put,
+  Delete,
+} from '@nestjs/common';
+import { MenuService } from './menu.service';
+import { BaseController } from 'src/base.controller';
+import { MenuSearchDto } from './dto/menu/menu-search.dto';
+import { Menu } from 'src/entity/menu.entity';
+
+@Controller('menu')
+export class MenuController extends BaseController {
+  constructor(private service: MenuService) {
+    super();
+  }
+
+  @Get('/:Menu_No')
+  getByID(@Param('Menu_No') Menu_No: string) {
+    return this.service.getByID(Menu_No);
+  }
+
+  @Post('search')
+  async search(@Body() body: MenuSearchDto) {
+    return this.service.search(body);
+  }
+
+  // @Post()
+  // async addMenu(@Body() data: um_Menu) {
+  //   try {
+  //     var is_success = await this.service.addMenu(data);
+  //     if (is_success) {
+  //       return { status: 0 };
+  //     } else {
+  //       return { status: 1 };
+  //     }
+  //   } catch (error) {
+  //     console.log(error);
+  //     return { status: 2, message: error.message };
+  //   }
+  // }
+
+  // @Put()
+  // async updateMenu(@Body() data: um_Menu) {
+  //   try {
+  //     var is_success = await this.service.updateMenu(data);
+  //     if (is_success) {
+  //       return { status: 0 };
+  //     } else {
+  //       return { status: 1 };
+  //     }
+  //   } catch (error) {
+  //     console.log(error);
+  //     return { status: 2, message: error.message };
+  //   }
+  // }
+
+  @Delete('/:Menu_No/')
+  async deleteateMenu(@Param('Menu_No') Menu_No: string) {
+    try {
+      const is_success = await this.service.deleteMenu(Menu_No);
+      if (is_success) {
+        return { status: 0 };
+      } else {
+        return { status: 1 };
+      }
+    } catch (error) {
+      console.log(error);
+      return { status: 2, message: error.message };
+    }
+  }
+
+  @Get('/get-main-menu/:Role_ID')
+  async getMainMenu(@Param('Role_ID') Role_ID: number) {
+    return this.service.getMainMenu(Role_ID);
+  }
+
+  @Post('save')
+  async saveAdd(@Body() data: Menu, @Request() req: any) {
+    data.createdBy = req.user.userId;
+    data.updatedBy = req.user.userId;
+    console.log(data);
+    return this.service.addMenu(data);
+  }
+
+  @Put('save/:id')
+  async saveEdit(
+    @Param('id') id: string,
+    @Body() data: Menu,
+    @Request() req: any,
+  ) {
+    data.updatedBy = req.user.userId;
+
+    return this.service.updateMenu(data);
+  }
+}
