a78fa5feece628fc7ebc239e24b641bc8e25c1c8
Note <note_kap@hotmail.com>
2024-09-25 16:20:02 +0700

    Refactor predefine data migration to update values for 'ConfigPath' and 'Is_Active' groups

--- src/migrations/1726026802491-SeedMenuData.ts
+++ src/migrations/1726026802491-SeedMenuData.ts
@@ -5,27 +5,26 @@ export class SeedMenuData1726026802491 implements MigrationInterface {
     await queryRunner.query(`
       INSERT INTO um_Menu (Menu_No, Menu_Seq, Menu_Name_EN, Menu_Name_TH, URL, Menu_Group, Menu_Icon, Is_MainMenu, Is_Active, Created_By, Created_Date, Updated_by, Updated_Date)
       VALUES 
-        ('M1000', 1, 'Common Master', 'ข้อมูลพื้นฐาน', NULL, NULL, 'mdi-cog', 'Y', 'Y', 1, GETDATE(), NULL, GETDATE()),
-        ('M1001', 1, 'User', 'ผู้ใช้งาน', '/user', 'M1000', NULL, 'N', 'Y', 1, GETDATE(), 1, GETDATE()),
-        ('M1002', 2, 'Role Permission', 'สิทธิการใช้งาน', '/role-permission', 'M1000', NULL, 'N', 'Y', 1, GETDATE(), NULL, GETDATE()),
-        ('M1003', 3, 'Predefine', 'Predefine', '/predefine', 'M1000', NULL, 'N', 'Y', 1, GETDATE(), NULL, GETDATE()),
-        ('M1004', 4, 'Menu', 'เมนู', '/menu', 'M1000', NULL, 'N', 'Y', 1, GETDATE(), NULL, GETDATE()),
-        ('M1005', 5, 'Log', 'Log', '/app-log', 'M1000', NULL, 'N', 'Y', 1, GETDATE(), NULL, GETDATE());
+        ('M1000', 1, 'Common Master', 'ข้อมูลพื้นฐาน', NULL, NULL, 'mdi-cog', 'Y', 'Y', 1, '2567-09-12 07:17:12.160', NULL, '2567-09-12 07:17:12.160'),
+        ('M1001', 1, 'User', 'ผู้ใช้งาน', '/common-master/user', 'M1000', NULL, 'N', 'Y', 1, '2567-09-12 07:17:12.160', 1, '2567-09-12 07:17:12.160'),
+        ('M1002', 2, 'Role Permission', 'สิทธิการใช้งาน', '/common-master/role-permission', 'M1000', NULL, 'N', 'Y', 1, '2567-09-12 07:17:12.160', NULL, '2567-09-12 07:17:12.160'),
+        ('M1003', 3, 'Predefine', 'Predefine', '/common-master/predefine', 'M1000', NULL, 'N', 'Y', 1, '2567-09-12 07:17:12.160', NULL, '2567-09-12 07:17:12.160'),
+        ('M1004', 4, 'Menu', 'เมนู', '/common-master/menu', 'M1000', NULL, 'N', 'Y', 1, '2567-09-12 07:17:12.160', NULL, '2567-09-12 07:17:12.160'),
+        ('M1005', 5, 'Log', 'Log', '/common-master/application-log', 'M1000', NULL, 'N', 'Y', 1, '2567-09-12 07:17:12.160', NULL, '2567-09-12 07:17:12.160');
     `);
 
     await queryRunner.query(`
-        SET IDENTITY_INSERT um_Menu_Route ON;
+      SET IDENTITY_INSERT um_Menu_Route ON;
       INSERT INTO um_Menu_Route (Menu_Route_ID, Menu_No, Route_Name, Route_Path, Physical_Path, Is_Require_Auth)
       VALUES 
-        (1, 'M1001', 'user', '/user', 'common-master/user/user-list.vue', 1),
+        (1, 'M1001', 'user', '/common-master/user', 'common-master/user/user-list.vue', 1),
         (2, 'M1001', 'user-info', '/user-info', 'common-master/user/user-info.vue', 1),
-        (3, 'M1002', 'role-permission', '/role-permission', 'common-master/role-permission/role-permission.vue', 1),
+        (3, 'M1002', 'role-permission', '/common-master/role-permission', 'common-master/role-permission/role-permission.vue', 1),
         (4, 'M1003', 'predefine', '/predefine', 'common-master/predefine/predefine.vue', 1),
         (5, 'M1004', 'menu', '/menu', 'common-master/menu/menu-list.vue', 1),
         (6, 'M1004', 'menu-info', '/menu-info', 'common-master/menu/menu-info.vue', 1),
         (7, 'M1005', 'app-log', '/app-log', 'common-master/app-log/app-log.vue', 1);
-
-        SET IDENTITY_INSERT um_Menu_Route OFF;
+      SET IDENTITY_INSERT um_Menu_Route OFF;
     `);
   }
 
--- src/migrations/1726026802492-SeedPredefineData.ts
+++ src/migrations/1726026802492-SeedPredefineData.ts
@@ -5,15 +5,15 @@ export class SeedPredefineData1726026802492 implements MigrationInterface {
     await queryRunner.query(`
       INSERT INTO co_Predefine (Predefine_Group, Predefine_CD, Description, Value_TH, Value_EN, Is_Active, Create_By, Create_Date, Update_By, Update_Date)
       VALUES 
-      ('Group1', 'CD1', 'Description1', 'ValueTH1', 'ValueEN1', '1', 1, CURRENT_TIMESTAMP, 1, CURRENT_TIMESTAMP),
-      ('Group2', 'CD2', 'Description2', 'ValueTH2', 'ValueEN2', '1', 1, CURRENT_TIMESTAMP, 1, CURRENT_TIMESTAMP),
-      ('Group3', 'CD3', 'Description3', 'ValueTH3', 'ValueEN3', '1', 1, CURRENT_TIMESTAMP, 1, CURRENT_TIMESTAMP);
+      ('ConfigPath', 'LOG', 'Log path', './logs', './logs', 'Y', NULL, '2567-09-24 07:50:08.237', NULL, '2567-09-24 07:50:08.237'),
+      ('Is_Active', 'N', '', 'Inactive', 'ไม่ใช้งาน', 'Y', NULL, '2567-09-13 05:45:44.687', NULL, '2567-09-13 05:45:44.687'),
+      ('Is_Active', 'Y', '', 'Active', 'ใช้งาน', 'Y', 1, '2567-09-13 03:44:58.587', 1, '2567-09-13 03:44:58.587');
     `);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
-      DELETE FROM co_Predefine WHERE Predefine_Group IN ('Group1', 'Group2', 'Group3') AND Predefine_CD IN ('CD1', 'CD2', 'CD3');
+      DELETE FROM co_Predefine WHERE Predefine_Group IN ('ConfigPath', 'Is_Active') AND Predefine_CD IN ('LOG', 'N', 'Y');
     `);
   }
 }
